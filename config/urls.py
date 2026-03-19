f7d25d3a2f3ac68870a8d722f1ef1322b60c1b62
r0lyi <rolysilvestre2@gmail.com>
2026-03-19 23:55:00 +0100

    chore: update setting/base + env authentication backen , cong resend ,conf  caching

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('apps.users.urls', namespace='auth')),
+    path('dashboard/', include('apps.dashboard.urls', namespace='dashboard')),
 ]
