45b7998e9f5a9a9a409c0800da7817d8c3529fb7
r0lyi <rolysilvestre2@gmail.com>
2026-04-18 01:52:33 +0200

    feat: /admin update

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,1 +1,8 @@
-"""Registros del admin reservados para utilidades de core."""
+"""Personalizacion global del Django Admin."""
+
+from django.contrib import admin
+
+
+admin.site.site_header = "LR Clean & Service | Administración"
+admin.site.site_title = "LR Clean & Service"
+admin.site.index_title = "Gestión interna del sistema"
