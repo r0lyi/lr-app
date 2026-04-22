401e03da7e7b58288805dd0330501f1ff5cee83f
r0lyi <rolysilvestre2@gmail.com>
2026-04-22 15:04:04 +0200

    feat: setting i18n y middleware para internalizacion

--- config/settings/components/i18n.py
+++ config/settings/components/i18n.py
@@ -1,7 +1,18 @@
 """Configuracion regional compartida."""
 
 
-LANGUAGE_CODE = "en-us"
+from config.settings.components.shared import BASE_DIR
+
+
+LANGUAGE_CODE = "es"
+
+LANGUAGES = [
+    ('es', 'Español'),
+    ('en', 'English'),
+]
+LOCALE_PATHS = [
+    BASE_DIR / 'locale',
+]
 TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
